0ff6973793f67386986cbe10b961c272ff8366cb
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-03-17 11:23:24 -0400

    Update BranchRule.vue

--- src/views/branch_rule/BranchRule.vue
+++ src/views/branch_rule/BranchRule.vue
@@ -28,7 +28,7 @@
 
                             <v-btn v-bind="props" class="text-subtitle-1  ml-12  " color="#E7E9E9" variant="flat"
                                 elevation="2" prepend-icon="mdi-plus-circle">
-                               Asignar regla de convivencia 
+                                Asignar regla de convivencia
                             </v-btn>
 
                         </template>
@@ -41,8 +41,9 @@
                                     <v-row>
                                         <v-col cols="12" md="12">
                                             <v-autocomplete v-model="editedItem.rule_id" :items="rules" clearable
-                                                label="Reglas de convivencia" prepend-inner-icon="mdi-ruler" item-title="name"
-                                                item-value="id" variant="underlined" :rules="selectRules"></v-autocomplete>
+                                                label="Reglas de convivencia" prepend-inner-icon="mdi-ruler"
+                                                item-title="name" item-value="id" variant="underlined"
+                                                :rules="selectRules"></v-autocomplete>  
                                         </v-col>
                                     </v-row>
                                     <v-divider></v-divider>
@@ -64,8 +65,8 @@
                     <v-dialog v-model="dialogDelete" max-width="500px">
                         <v-card>
                             <v-toolbar color="red">
-                                <span class="text-subtitle-2 ml-4"> Eliminar la asignación de esta regla de convivencia a esta
-                                    sucursal</span>
+                                <span class="text-subtitle-2 ml-4"> Eliminar la asignación de esta regla de convivencia a
+                                    esta sucursal</span>
                             </v-toolbar>
 
                             <v-card-text class="mt-2 mb-2"> ¿Desea deshacer la asignación?</v-card-text>
@@ -110,20 +111,9 @@
                     <v-divider class="mx-4" inset vertical></v-divider>
                     <v-spacer></v-spacer>
                 </template>
-                <!--<template v-slot:item.name="{ item }">
 
-                    <v-avatar class="mr-5" elevation="3" color="grey-lighten-4">
-                        <v-img :src="'http://127.0.0.1:8000/api/images/' + item.image_service" alt="image"></v-img>
-                    </v-avatar>
-                    {{ item.name }}
-                </template>-->
                 <template v-slot:item.actions="{ item }">
-                    <!--<v-icon size="25" color="blue" class="me-2" @click="editItem(item)">
-         mdi-pencil
-       </v-icon>
-                    <v-icon size="25" color="green" @click="showProfessionals(item)">
-                        mdi-account-tie-outline
-                    </v-icon>-->
+
                     <v-icon size="25" color="red" @click="deleteItem(item)">
                         mdi-delete
                     </v-icon>
@@ -131,92 +121,7 @@
             </v-data-table>
         </v-card-text>
 
-        <!--Professionals-->
-        <!--<v-dialog v-model="dialogProfessionals" fullscreen transition="dialog-bottom-transition">
-            <v-card>
-                <v-toolbar color="#F18254">
-                    <span class="text-h6 ml-4"> Trabajadores que realizan servicio</span>
-                    <v-spacer></v-spacer>
-                    <v-btn class="text-subtitle-1  ml-12" color="#E7E9E9" variant="flat"
-                        @click="this.dialogAddProfessionals = true">
-                        Asignar Trabajador
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text class="mt-2 mb-2">
-                    <v-text-field class="mt-1 mb-1" v-model="search2" append-icon="mdi-magnify" label="Buscar" single-line
-                        hide-details></v-text-field>
-                    <v-data-table :headers="headers2" :items="branchServiceProfessionals" :search="search2"
-                        class="elevation-1">
 
-                        <template v-slot:item.actions="{ item }">
-                            <v-icon size="25" color="red" @click="closeProfessionalRequest(item)">
-                                mdi-delete
-                            </v-icon>
-                        </template>
-
-                    </v-data-table>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="#E7E9E9" variant="flat" @click="closeDelete">
-                        Volver
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-
-        <v-dialog v-model="dialogAddProfessionals" width="500">
-        <v-card>
-          <v-toolbar color="#F18254">
-            <span class="text-subtitle-2 ml-4">Asignar Trabajador</span>
-          </v-toolbar>
-          <v-card-text class="mt-2 mb-2">
-            <v-form ref="form" v-model="valid" enctype="multipart/form-data">
-              <v-container>
-                <v-row>
-                  <v-col cols="12" md="12">
-                    <v-autocomplete v-model="professional_id" :items="professionals" label="Profesional"
-                      prepend-icon="mdi-account-tie-outline" item-title="name" item-value="id" variant="underlined"
-                      :rules="selectRules"></v-autocomplete>
-                  </v-col>
-                </v-row>
-              </v-container>
-              <v-divider></v-divider>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="#E7E9E9" variant="flat" @click="closeProfessional">
-                  Cancelar
-                </v-btn>
-                <v-btn color="#F18254" variant="flat" @click="saveProfessional" :disabled="!valid">
-                  Aceptar
-                </v-btn>
-              </v-card-actions>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-dialog>
-      <v-dialog v-model="dialogDeleteProfessional" width="500">
-        <v-card>
-
-          <v-toolbar color="red">
-            <span class="text-subtitle-2 ml-4"> Eliminar Servicio asignado a profesional</span>
-          </v-toolbar>
-
-          <v-card-text class="mt-2 mb-2"> ¿Desea eliminar este servicio a este professional?</v-card-text>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="#E7E9E9" variant="flat" @click="closerequestProfessional">
-              Cancelar
-            </v-btn>
-            <v-btn color="#F18254" variant="flat" @click="professionalsDelete">
-              Aceptar
-            </v-btn>
-
-          </v-card-actions>
-        </v-card>
-      </v-dialog>-->
     </v-card>
 
 
@@ -410,82 +315,6 @@ export default {
             this.close();
 
         },
-
-        //professionals
-
-        /*showProfessionals(item) {
-            this.branchServiceSelect = item;
-            console.log(this.branchServiceSelect);
-            this.branch_service_id = item.id;
-            console.log(item.id);
-            axios
-                .get('http://127.0.0.1:8000/api/branch-service-professionals', {
-                    params: {
-                        branch_service_id: item.id
-                    }
-                })
-                .then((response) => {
-                    this.branchServiceProfessionals = response.data.professionals;
-                    console.log('imprime permissions');
-                });
-                axios
-                .get('http://127.0.0.1:8000/api/professionals-branch-service', {
-                    params: {
-                        branch_service_id: item.id
-                    }
-                })
-                .then((response) => {
-                    this.professionals = response.data.professionals;
-                });
-            this.dialogProfessionals = true;
-        },
-        /*closeProfessional() {
-      this.dialogAddProfessionals = false;
-      this.branch_service_id = '';
-      this.showProfessionals(this.branchServiceSelect);
-    },
-    /*saveProfessional() {
-      this.valid = false,
-        this.data.professional_id = this.professional_id;
-      this.data.branch_service_id = this.branch_service_id;
-      axios
-        .post('http://127.0.0.1:8000/api/professionalservice', this.data)
-        .then(() => {
-         this.professional_id = '',
-          this.dialogAddProfessionals = false;
-          this.showProfessionals(this.branchServiceSelect);
-          this.showAlert("success", "Permiso asignado correctamente", 3000);
-        })
-    },
-    /*closeProfessionalRequest(item) {
-      console.log(item);
-      this.dialogDeleteProfessional = true
-      //this.editedItem.branch_id=item.id
-      this.professional_id = item.professional_id;
-      this.branch_service_id = item.branch_service_id;
-    },
-    closerequestProfessional() {
-      this.dialogDeleteProfessional = false;
-      this.branch_service_id = '',
-         this.professional_id_id = '',
-      this.showPermission(this.branchServiceSelect)
-    },*/
-    /*professionalsDelete() {
-      let request = {
-        professional_id: this.professional_id,
-        branch_service_id: this.branch_service_id
-      };
-      axios
-        .post('http://127.0.0.1:8000/api/professionalservice-destroy', request)
-        .then(() => {
-          this.dialogDeleteProfessional = false
-          this.charge_id = '',
-         this.permission_id = '',
-          console.log(this.branchServiceSelect);
-          this.showProfessionals(this.branchServiceSelect)
-          this.showAlert("success", "Asignación de profesional a servicio hecha correctamente", 3000)
-        })
-    },*/
     },
 }
 </script>
