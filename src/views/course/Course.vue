87c23671cab61c729c00f28f9a8a52158178b8d1
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-04-07 18:56:18 -0400

    Asignar Productos a estudiantes

--- src/views/course/Course.vue
+++ src/views/course/Course.vue
@@ -35,7 +35,7 @@
               </template>
               <v-card>
                 <v-toolbar color="#F18254">
-                  <span class="text-subtitle-2 ml-4"> Curso</span>
+                  <span class="text-subtitle-2 ml-4"> {{formTitle}}</span>
                 </v-toolbar>
                 <v-card-text>
                   <v-form v-model="valid" enctype="multipart/form-data">
@@ -211,6 +211,8 @@
             elevation="1" class="mr-1 mt-1 mb-1" title="Editar Curso"></v-btn>
             <v-btn density="comfortable" icon="mdi-account-school"  @click="showStudents(item)" color="green" variant="tonal"
             elevation="1" class="mr-1 mt-1 mb-1" title="Estudiantes inscritos"></v-btn>
+            <v-btn density="comfortable" class="mr-1 mt-1 mb-1" icon="mdi-storefront" @click="showProducts(item)" color="orange-darken-1" variant="tonal"
+            elevation="1" title="Vender productos a estudiantes"></v-btn>
           <v-btn density="comfortable" icon="mdi-delete" @click="deleteItem(item)" color="red-darken-4" variant="tonal"
             elevation="1" title="Eliminar Curso"></v-btn>
           </template>
@@ -222,7 +224,7 @@
         <v-dialog v-model="dialogUpdateS" max-width="700px">
           <v-card>
             <v-toolbar color="#F18254">
-              <span class="text-subtitle-2 ml-4">Actualizar Datos</span>
+              <span class="text-subtitle-2 ml-4">Actualizar Datos {{formTitle}}</span>
             </v-toolbar>
             <v-card-text>
               <v-form v-model="valid" enctype="multipart/form-data">
@@ -279,9 +281,7 @@
           </v-card>
         </v-dialog>
 
-        <!--Professionals-->
-
-
+        <!--Students-->
         <v-dialog v-model="dialogStudents" fullscreen transition="dialog-bottom-transition">
           <v-card>
             <v-toolbar color="#F18254">
@@ -416,6 +416,121 @@
           </v-card-text>
         </v-card>
     </v-dialog>
+
+    <!--Venta Productos-->
+    <v-dialog v-model="dialogProducts" fullscreen transition="dialog-bottom-transition">
+        <v-card>
+          <v-toolbar color="#F18254">
+            <span class="text-subtitle-1 ml-4">Productos vendidos a estudiantes del curso</span>
+            <v-spacer></v-spacer>
+            <v-btn class="text-subtitle-1  ml-12" color="#E7E9E9" variant="flat" @click="this.dialogAddProduct = true">
+              Asignar Producto
+            </v-btn>
+          </v-toolbar>
+          <v-card-text class="mt-2 mb-2">
+            <v-text-field class="mt-1 mb-1" v-model="search3" append-icon="mdi-magnify" label="Buscar" single-line
+              hide-details></v-text-field>
+            <v-data-table :headers="headers3" :items="productSales" :search="search3" class="elevation-1" :items-per-page-text="'Elementos por páginas'" no-results-text="No hay datos disponibles" no-data-text="No hay datos disponibles" >
+              <!--<template v-slot:group-header="{ item, columns, toggleGroup, isGroupOpen }">:group-by="groupBy"
+              <tr>
+                <td :colspan="columns.length">
+                  <VBtn size="small" variant="text" :icon="isGroupOpen(item) ? '$expand' : '$next'"
+                    @click="toggleGroup(item)"></VBtn>
+                  {{ item.value }}
+                </td>
+              </tr>
+            </template>-->
+            <template v-slot:item.nameProduct="{ item }">
+
+            <v-avatar class="mr-5" elevation="3" color="grey-lighten-4">
+              <v-img :src="'http://127.0.0.1:8000/api/images/' + item.image_product" alt="image"></v-img>
+            </v-avatar>
+            {{ item.nameProduct }}
+            </template>
+
+            <template v-slot:item.nameStudent="{ item }">
+
+            <v-avatar class="mr-5" elevation="3" color="grey-lighten-4">
+              <v-img :src="'http://127.0.0.1:8000/api/images/' + item.student_image" alt="image"></v-img>
+            </v-avatar>
+            {{ item.nameStudent }}
+            </template>
+
+              <template v-slot:item.actions="{ item }">
+                <!--<v-btn density="comfortable" icon="mdi-pencil"  @click="editItemProduct(item)" color="primary" variant="tonal"
+            elevation="1" class="mr-1 mt-1 mb-1" title="Editar existencia"></v-btn>-->
+          <v-btn density="comfortable" icon="mdi-delete" @click="closeproductRequest(item)" color="red-darken-4" variant="tonal"
+            elevation="1" title="Eliminar existencia de producto"></v-btn>
+                <!--<v-icon size="small" color="red" @click="closestoreRequest(item)">
+                  mdi-delete
+                </v-icon>-->
+              </template>
+
+            </v-data-table>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="#E7E9E9" variant="flat" @click="closeDelete">
+              Volver
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialogAddProduct" width="500">
+        <v-card>
+          <v-toolbar color="#F18254">
+            <span class="text-subtitle-2 ml-4">{{formTitle}}</span>
+          </v-toolbar>
+          <v-card-text class="mt-2 mb-2">
+            <v-form ref="form" v-model="valid" enctype="multipart/form-data">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="12">
+                    <v-autocomplete v-model="student_id" :items="studentsCourse" label="Estudiantes"
+                      prepend-icon="mdi-account-tie-outline" item-title="name" item-value="id" variant="underlined"
+                      :rules="selectRules"></v-autocomplete>
+                  <v-autocomplete v-model="product_id" :items="products" clearable label="Productos"
+                        prepend-icon="mdi-tag" item-title="name" item-value="id" variant="underlined"
+                        :rules="selectRules"></v-autocomplete>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="#E7E9E9" variant="flat" @click="closeproduct">
+                  Cancelar
+                </v-btn>
+                <v-btn color="#F18254" variant="flat" @click="saveProduct" :disabled="!valid">
+                  Aceptar
+                </v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialogRequestProduct" width="500">
+        <v-card>
+
+          <v-toolbar color="red">
+            <span class="text-subtitle-2 ml-4"> Eliminar asignación de producto</span>
+          </v-toolbar>
+
+          <v-card-text class="mt-2 mb-2"> ¿Desea eliminar la asignación del producto?</v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="#E7E9E9" variant="flat" @click="closerequestProduct">
+              Cancelar
+            </v-btn>
+            <v-btn color="#F18254" variant="flat" @click="deleteProduct">
+              Aceptar
+            </v-btn>
+
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
   </v-container>
 </template>
 <script>
@@ -456,6 +571,24 @@ export default {
     search2: '',
     isDatePickerOpen: false,
     selectedDate: null,
+    //venta productos
+    dialogProducts: false,
+    productSales: [],
+    search3: '',
+    studentsCourse: [],
+    dialogAddProduct: false,
+    enrollment_id: '',
+    dialogRequestProduct: false,
+    products: [],
+    product_id:'',
+    productsale_id: '',
+    headers3: [
+      { title: 'Nombre Estudiante', key: 'nameStudent' },
+      { title: 'Nombre Producto', key: 'nameProduct' },
+      { title: 'Precio del producto', key: 'price' },
+      { title: 'Fecha de venta', key: 'data' },
+      { title: 'Acciones', key: 'actions', sortable: false },
+    ],
     headers: [
       { title: 'Nombre', key: 'name' },
       { title: 'Prec. Reserva', key: 'reservation_price' },
@@ -480,7 +613,7 @@ export default {
 
     results: [],
     courseStudents: [],
-    editedIndex: -1,
+    editedIndex: 1,
     users: [],
     file: '',
     imgMiniatura: '',
@@ -540,6 +673,7 @@ export default {
       id:'',
 
     },
+
     selectRules: [(v) => !!v || "Seleccionar al menos un elemeto"],
   }),
   setup() {
@@ -560,7 +694,19 @@ export default {
     },
 
     formTitle() {
-      return this.editedIndex === -1 ? 'Nuevo Curso' : 'Editar Curso'
+      if (this.editedIndex == 3) {
+        return 'Asignar producto a estudiante';
+      }
+      if (this.editedIndex == 2) {
+        return 'Editar Curso';
+      }
+      if (this.editedIndex == 1) {
+        return 'Nuevo curso';
+      }
+      else{
+        return 'Asiganar estudiante al curso'
+      }
+      //return this.editedIndex === -1 ? 'Nuevo Curso' : 'Editar Curso'
     },
     /*dateFormatted() {
       const date = this.input ? new Date(this.input) : new Date();
@@ -838,7 +984,7 @@ export default {
     editItem(item) {
       this.file = '';
       this.imgMiniatura = 'http://127.0.0.1:8000/api/images/' + item.course_image;
-      this.editedIndex = 1;
+      this.editedIndex = 2;
       this.editedItem = Object.assign({}, item);
       this.editedItem.startDate = this.parseDate(item.startDate);
       this.editedItem.endDate = this.parseDate(item.endDate);
@@ -869,7 +1015,7 @@ export default {
       this.dialog = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
+        this.editedIndex = 1
         this.imgMiniatura = '';
         this.file = '';
       })
@@ -885,16 +1031,17 @@ export default {
       this.dialogStudents = false;
       this.dialogDelete = false;
       this.dialogRequest = false;
+      this.dialogProducts = false;
       this.imgMiniatura = '';
       this.file = '';
+        this.editedIndex = 1
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedStudent = Object.assign({}, this.defaultStudent)
-        this.editedIndex = -1
       })
     },
     save() {
-      if (this.editedIndex > -1) {
+      if (this.editedIndex == 2) {
         this.valid = false;
         /*this.data.id = this.editedItem.id;
         this.data.name = this.editedItem.name;
@@ -916,8 +1063,9 @@ export default {
             this.initialize();
             this.imgMiniatura = '';
             this.file = '';
+            
           })
-      } else {
+      } if (this.editedIndex == 1) {
         this.valid = false;
         /*this.data.name = this.editedItem.name;
         this.data.name = this.editedItem.name;
@@ -942,6 +1090,122 @@ export default {
       }
       this.close()
     },
-  },
+    //venta productos
+    showProducts(item) {
+      this.courseSelect = item;
+      console.log('this.courseSelect');
+      console.log(this.courseSelect);
+      this.course_id = item.id;
+      this.enrollment_id = item.enrollment_id;
+      console.log(item.id);
+      axios
+        .get('http://127.0.0.1:8000/api/productsale-show', {
+          params: {
+            course_id: item.id,
+            enrollment_id: item.enrollment_id
+          }
+        })
+        .then((response) => {
+          this.productSales = response.data.productsales;
+          console.log('imprime estudiantes');
+          console.log(response.data.productSales);
+
+        });
+        axios
+        .get('http://127.0.0.1:8000/api/course-student-product-show',{
+          params: {
+            course_id: item.id
+          }
+        })
+        .then((response) => {
+          this.studentsCourse = response.data.students;
+        });
+        axios
+        .get('http://127.0.0.1:8000/api/products-academy-show', {
+          params: {
+            enrollment_id: item.enrollment_id
+          }
+        })
+        .then((response) => {
+          this.products = response.data.products;
+          console.log('imprime productos');
+          console.log(response.data.products);
+
+        });
+        this.editedIndex = 3;
+      this.dialogProducts = true;
+    },
+    closeproduct() {
+      this.dialogAddProduct = false;
+      this.product_id = '';
+      this.student_id = '';
+      this.showProducts(this.courseSelect)
+    },
+    saveProduct() {
+      if (this.editedIndex == 3) {
+        this.valid = false,
+        this.data.enrollment_id = this.enrollment_id;
+      this.data.student_id = this.student_id;
+      this.data.id = this.product_id;
+      this.data.cant = 1;
+      console.log('this.data');
+      console.log(this.data);
+      axios
+          .post('http://127.0.0.1:8000/api/productsale', this.data)
+          .then(() => {
+          this.dialogAddProduct = false;
+          this.student_id_id = '',
+          this.product_id = '';
+          this.showProducts(this.courseSelect);
+          this.showAlert("success", "Producto asignado correctamente al estudiante", 3000);
+        })
+      }
+      /*if (this.editedIndex == 4){
+        this.valid = false,
+        this.data.enrollment_id = this.enrollment_id;
+      this.data.store_id = this.store_id;
+      this.data.product_id = this.product_id;
+      this.data.product_quantity = this.product_quantity;
+      console.log('this.data');
+      console.log(this.data);
+      axios
+          .put('http://127.0.0.1:8000/api/productstore', this.data)
+          .then(() => {
+          this.dialogAddProduct = false;
+          this.store_id = '',
+          this.product_id = '';
+          this.product_quantity = '';
+          this.showProducts(this.enrollmentSelect);
+          this.showAlert("success", "Asignacion editada correctamente", 3000);
+        })
+      }*/
+    },
+    closeproductRequest(item) {
+      this.dialogRequestProduct = true
+      //this.editedItem.branch_id=item.id
+      this.productsale_id = item.id;
+
+    },
+    closerequestProduct() {
+      this.dialogRequestProduct = false;
+      this.productsale_id = '';
+      this.showProducts(this.courseSelect)
+    },
+    deleteProduct() {
+      let request = {
+        id: this.productsale_id
+      };
+      axios
+        .post('http://127.0.0.1:8000/api/productsale-destroy', request)
+        .then(() => {
+          this.dialogRequestProduct = false;
+          this.productsale_id = '',
+          console.log('this.courseSelect');
+          console.log(this.courseSelect);
+          this.showProducts(this.courseSelect);
+          this.showAlert("success", "Asignación eliminada correctamente", 3000);    
+        })
+    },
+  },//endMethods
 }
 </script>
